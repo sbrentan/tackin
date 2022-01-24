51b4726644b138ca2d3e7c5a98e326813432e2ad
Matteo <matteo68.mc@gmail.com>
2022-01-24 14:35:34 +0100

    Fix rospy publish

--- src/test2_control/scripts/grasper.py
+++ src/test2_control/scripts/grasper.py
@@ -28,67 +28,94 @@ class Joint(Enum):
     H1_F3J3 = 11
 
 def init():
-    # global shoulder_pan_pub, shoulder_lift_pub, elbow_pub, wrist1_pub, wrist2_pub, wrist3_pub
-    # global H1_F1J2_pub, H1_F1J3_pub, H1_F2J2_pub, H1_F2J3_pub, H1_F3J2_pub, H1_F3J3_pub, rate
-    # shoulder_pan_pub = rospy.Publisher('/shoulder_pan_joint_position_controller/command', Float64, queue_size=10)
-    # shoulder_lift_pub = rospy.Publisher('/shoulder_lift_joint_position_controller/command', Float64, queue_size=10)
-    # elbow_pub = rospy.Publisher('/elbow_joint_position_controller/command', Float64, queue_size=10)
-    # wrist1_pub = rospy.Publisher('/wrist_1_joint_position_controller/command', Float64, queue_size=10)
-    # wrist2_pub = rospy.Publisher('/wrist_2_joint_position_controller/command', Float64, queue_size=10)
-    # wrist3_pub = rospy.Publisher('/wrist_3_joint_position_controller/command', Float64, queue_size=10)
-    # H1_F1J2_pub = rospy.Publisher('/H1_F1J2_joint_position_controller/command', Float64, queue_size=10)
-    # H1_F1J3_pub = rospy.Publisher('/H1_F1J3_joint_position_controller/command', Float64, queue_size=10)
-    # H1_F2J2_pub = rospy.Publisher('/H1_F2J2_joint_position_controller/command', Float64, queue_size=10)
-    # H1_F2J3_pub = rospy.Publisher('/H1_F2J3_joint_position_controller/command', Float64, queue_size=10)
-    # H1_F3J2_pub = rospy.Publisher('/H1_F3J2_joint_position_controller/command', Float64, queue_size=10)
-    # H1_F3J3_pub = rospy.Publisher('/H1_F3J3_joint_position_controller/command', Float64, queue_size=10)
+    rospy.init_node('supreme_commander')
+
+    global shoulder_pan_pub, shoulder_lift_pub, elbow_pub, wrist1_pub, wrist2_pub, wrist3_pub
+    global H1_F1J2_pub, H1_F1J3_pub, H1_F2J2_pub, H1_F2J3_pub, H1_F3J2_pub, H1_F3J3_pub, rate
+    shoulder_pan_pub = rospy.Publisher('/shoulder_pan_joint_position_controller/command', Float64, queue_size=10)
+    shoulder_lift_pub = rospy.Publisher('/shoulder_lift_joint_position_controller/command', Float64, queue_size=10)
+    elbow_pub = rospy.Publisher('/elbow_joint_position_controller/command', Float64, queue_size=10)
+    wrist1_pub = rospy.Publisher('/wrist_1_joint_position_controller/command', Float64, queue_size=10)
+    wrist2_pub = rospy.Publisher('/wrist_2_joint_position_controller/command', Float64, queue_size=10)
+    wrist3_pub = rospy.Publisher('/wrist_3_joint_position_controller/command', Float64, queue_size=10)
+    H1_F1J2_pub = rospy.Publisher('/H1_F1J2_joint_position_controller/command', Float64, queue_size=10)
+    H1_F1J3_pub = rospy.Publisher('/H1_F1J3_joint_position_controller/command', Float64, queue_size=10)
+    H1_F2J2_pub = rospy.Publisher('/H1_F2J2_joint_position_controller/command', Float64, queue_size=10)
+    H1_F2J3_pub = rospy.Publisher('/H1_F2J3_joint_position_controller/command', Float64, queue_size=10)
+    H1_F3J2_pub = rospy.Publisher('/H1_F3J2_joint_position_controller/command', Float64, queue_size=10)
+    H1_F3J3_pub = rospy.Publisher('/H1_F3J3_joint_position_controller/command', Float64, queue_size=10)
+
     # rospy.init_node('supreme_commander', anonymous=True)
     # rate = rospy.Rate(1000) # 10hz
 
-    rospy.init_node('supreme_commander')
-
     global attach_srv, detach_srv
-    rospy.loginfo("Creating ServiceProxy to /link_attacher_node/attach")
+    #rospy.loginfo("Creating ServiceProxy to /link_attacher_node/attach")
     attach_srv = rospy.ServiceProxy('/link_attacher_node/attach', Attach)
     attach_srv.wait_for_service()
     rospy.loginfo("Created ServiceProxy to /link_attacher_node/attach")
 
-    rospy.loginfo("Creating ServiceProxy to /link_attacher_node/detach")
+    #rospy.loginfo("Creating ServiceProxy to /link_attacher_node/detach")
     detach_srv = rospy.ServiceProxy('/link_attacher_node/detach', Attach)
     detach_srv.wait_for_service()
     rospy.loginfo("Created ServiceProxy to /link_attacher_node/detach")
 
+    time.sleep(1)
+    rospy.loginfo("Created all the publishers")
 
 
-
-def move(joint, position):
-    if(joint == Joint.SHOULDER_PAN):
-        os.system('rostopic pub -1 /shoulder_pan_joint_position_controller/command std_msgs/Float64 "data: '+str(position)+'" &')
-    elif(joint == Joint.SHOULDER_LIFT):
-        os.system('rostopic pub -1 /shoulder_lift_joint_position_controller/command std_msgs/Float64 "data: '+str(position)+'" &')
-    elif(joint == Joint.ELBOW):
-        os.system('rostopic pub -1 /elbow_joint_position_controller/command std_msgs/Float64 "data: '+str(position)+'" &')
-    elif(joint == Joint.WRIST1):
-        os.system('rostopic pub -1 /wrist_1_joint_position_controller/command std_msgs/Float64 "data: '+str(position)+'" &')
-    elif(joint == Joint.WRIST2):
-        os.system('rostopic pub -1 /wrist_2_joint_position_controller/command std_msgs/Float64 "data: '+str(position)+'" &')
-    elif(joint == Joint.WRIST3):
-        os.system('rostopic pub -1 /wrist_3_joint_position_controller/command std_msgs/Float64 "data: '+str(position)+'" &')
-    elif(joint == Joint.H1_F1J2):
-        os.system('rostopic pub -1 /H1_F1J2_joint_position_controller/command std_msgs/Float64 "data: '+str(position)+'" &')
-    elif(joint == Joint.H1_F1J3):
-        os.system('rostopic pub -1 /H1_F1J3_joint_position_controller/command std_msgs/Float64 "data: '+str(position)+'" &')
-    elif(joint == Joint.H1_F2J2):
-        os.system('rostopic pub -1 /H1_F2J2_joint_position_controller/command std_msgs/Float64 "data: '+str(position)+'" &')
-    elif(joint == Joint.H1_F2J3):
-        os.system('rostopic pub -1 /H1_F2J3_joint_position_controller/command std_msgs/Float64 "data: '+str(position)+'" &')
-    elif(joint == Joint.H1_F3J2):
-        os.system('rostopic pub -1 /H1_F3J2_joint_position_controller/command std_msgs/Float64 "data: '+str(position)+'" &')
-    elif(joint == Joint.H1_F3J3):
-        os.system('rostopic pub -1 /H1_F3J3_joint_position_controller/command std_msgs/Float64 "data: '+str(position)+'" &')
+#def move(joint, position):
+#    if(joint == Joint.SHOULDER_PAN):
+#        os.system('rostopic pub -1 /shoulder_pan_joint_position_controller/command std_msgs/Float64 "data: '+str(position)+'" &')
+#    elif(joint == Joint.SHOULDER_LIFT):
+#        os.system('rostopic pub -1 /shoulder_lift_joint_position_controller/command std_msgs/Float64 "data: '+str(position)+'" &')
+#    elif(joint == Joint.ELBOW):
+#        os.system('rostopic pub -1 /elbow_joint_position_controller/command std_msgs/Float64 "data: '+str(position)+'" &')
+#    elif(joint == Joint.WRIST1):
+#        os.system('rostopic pub -1 /wrist_1_joint_position_controller/command std_msgs/Float64 "data: '+str(position)+'" &')
+#    elif(joint == Joint.WRIST2):
+#        os.system('rostopic pub -1 /wrist_2_joint_position_controller/command std_msgs/Float64 "data: '+str(position)+'" &')
+#    elif(joint == Joint.WRIST3):
+#        os.system('rostopic pub -1 /wrist_3_joint_position_controller/command std_msgs/Float64 "data: '+str(position)+'" &')
+#    elif(joint == Joint.H1_F1J2):
+#        os.system('rostopic pub -1 /H1_F1J2_joint_position_controller/command std_msgs/Float64 "data: '+str(position)+'" &')
+#    elif(joint == Joint.H1_F1J3):
+#        os.system('rostopic pub -1 /H1_F1J3_joint_position_controller/command std_msgs/Float64 "data: '+str(position)+'" &')
+#    elif(joint == Joint.H1_F2J2):
+#        os.system('rostopic pub -1 /H1_F2J2_joint_position_controller/command std_msgs/Float64 "data: '+str(position)+'" &')
+#    elif(joint == Joint.H1_F2J3):
+#        os.system('rostopic pub -1 /H1_F2J3_joint_position_controller/command std_msgs/Float64 "data: '+str(position)+'" &')
+#    elif(joint == Joint.H1_F3J2):
+#        os.system('rostopic pub -1 /H1_F3J2_joint_position_controller/command std_msgs/Float64 "data: '+str(position)+'" &')
+#    elif(joint == Joint.H1_F3J3):
+#        os.system('rostopic pub -1 /H1_F3J3_joint_position_controller/command std_msgs/Float64 "data: '+str(position)+'" &')
 
     # time.sleep(0.2)
 
+def move(joint, position):
+    if(joint == Joint.SHOULDER_PAN):
+        shoulder_pan_pub.publish(position)
+    elif(joint == Joint.SHOULDER_LIFT):
+        shoulder_lift_pub.publish(position)
+    elif(joint == Joint.ELBOW):
+        elbow_pub.publish(position)
+    elif(joint == Joint.WRIST1):
+        wrist1_pub.publish(position)
+    elif(joint == Joint.WRIST2):
+        wrist2_pub.publish(position)
+    elif(joint == Joint.WRIST3):
+        wrist3_pub.publish(position)
+    elif(joint == Joint.H1_F1J2):
+        H1_F1J2_pub.publish(position)
+    elif(joint == Joint.H1_F1J3):
+        H1_F1J3_pub.publish(position)
+    elif(joint == Joint.H1_F2J2):
+        H1_F2J2_pub.publish(position)
+    elif(joint == Joint.H1_F2J3):
+        H1_F2J3_pub.publish(position)
+    elif(joint == Joint.H1_F3J2):
+        H1_F3J2_pub.publish(position)
+    elif(joint == Joint.H1_F3J3):
+        H1_F3J3_pub.publish(position)
 
 def reset():
     move(Joint.SHOULDER_PAN, 0)
